951a241adf468245bd52a52f65af059199836c53
Mohamed-Gamal-22 <mogamall086@gmail.com>
2026-08-07 19:26:39 +0300

    firsst part of auth

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 const GUEST_ONLY = ["/login", "/register", "/forgot-password"];
 
 export default withAuth(
-  function middleware(req) {
+  function proxy(req) {
     const token = req.nextauth.token;
     const { pathname } = req.nextUrl;
     const isLoggedIn = Boolean(token);
